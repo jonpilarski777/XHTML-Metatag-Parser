cd0138e28d7970bec939df43a9585b685fce6b55
Jon <jonpilarski927@gmail.com>
2022-02-15 07:52:25 -0600

    Create components

---
+++ xhtml-metatag-parser/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import HTMLInputBox from "./HTMLInputBox";
+import MetaTagTable from './MetaTagTable';
+
+function App() {
+  return (
+  <div>
+    <h1>Metatag-Parser</h1>
+    <Header />
+    <HTMLInputBox />
+    <MetaTagTable />
+  
+  </div>
+  );
+}
+
+export default App;
