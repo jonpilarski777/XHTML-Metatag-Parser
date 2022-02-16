25cc96b53e9a74eeade41ed97dbbe5d201d0f89f
Jon <jonpilarski927@gmail.com>
2022-02-16 09:19:20 -0600

    Add state to Textfield

--- xhtml-metatag-parser/src/components/HtmlInputBox.js
+++ xhtml-metatag-parser/src/components/HtmlInputBox.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TextField from '@mui/material/TextField'
 
-
-//<Textareasize> component could also have been used but textfield produced 
 function HtmlInputBox() {
 
+  //Using dot notation to handle colon in key names
+
+  const [htmlTextSnippet, setHtmlTextSnippet] = useState({})
+
+  // const [metaTagProperties, setMetaTagProperties] = useState({
+  //   ['dcterms:conformsTo']:'',
+  //   ['a11y:certifiedBy']: '',
+  //   ['a11y:certifierCredential']: '',
+  //   ['a11y:certifierReport']: '',
+  // })
+  // setMetaTagProperties({...metaTagProperties, [e.target.name]:[e.target.value]})
+  
+  console.log(htmlTextSnippet)
+
+  function handleChange(e) {
+
+    setHtmlTextSnippet({...htmlTextSnippet, [e.target.name]:[e.target.value]})
+  
+  }
+ 
+  // // function handleSubmit (e) {
+  // //   e.preventDefault()
+  // //   if (metaTagProperties) {
+  // //     console.log(metaTagProperties)
+  // //   }
+  // // }
+
+  // function extractProperties(e) {
+
+  // }
 
   return (
-    <div>
-      <TextField placeholder="Paste your HTML sample text in this box" multiline rows={9} maxRows={Infinity}/>
-    </div>
+    <form autoComplete="off">
+      <TextField 
+      label ="Paste your sample text in this box" 
+      multiline 
+      helperText = "Paste your sample HTML sample inside of this box"
+      minRows={9} 
+      maxRows={Infinity}
+      onChange={handleChange}
+      />
+    </form>
   )
 }
 
